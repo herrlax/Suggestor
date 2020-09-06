24420709b49b57aa2bbb045c3c51c02b64802de2
Mikael Malmqvist <mikael@malmqvist.it>
2020-09-06 18:52:43 +0200

    Cleaned up App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import { useSongState } from "./context/song";
 import { useUserState, useUserActions } from "./context/user";
 
 const App = () => {
-  // const { currentSong } = useSongState();
   const { data } = useUserState();
   const { logIn } = useUserActions();
 
-  return <>{typeof data === "undefined" && <button onClick={logIn}>Login</button>}</>;
-  // return <>Current song: {currentSong && currentSong.name}</>;
+  return typeof data === "undefined" ? (
+    <button onClick={logIn}>Login</button>
+  ) : (
+    <span>You're logged in</span>
+  );
 };
 
 export default App;
